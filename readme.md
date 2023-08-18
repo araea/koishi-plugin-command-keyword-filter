5e36354201ac5cdc2299666a4c57aa25b2a7791c
Nawyjx <3373167460@qq.com>
2023-08-18 16:23:52 +0800

    Update

--- readme.md
+++ readme.md
@@ -2,4 +2,43 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-command-keyword-filter?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-command-keyword-filter)
 
+## 🎈 介绍
 
+👋 你好，command-keyword-filter 是一个插件，用于过滤用户在触发指令之前传递的参数，以确定是否包含预设的关键词。它能够让你的机器人有选择性地执行指令。🤖
+
+## 功能介绍
+
+- 📝 你可以设置一些关键词，当用户输入这些关键词时，会触发一些动作，比如封印用户、提示用户、或者既封印又提示用户。
+- ⏱️ 你可以设置一个时间限制，当用户触发关键词后，在这个时间内，机器人会忽略用户的指令输入，根据设置条件给出一些提示信息。
+- 🎨 你可以自定义触发关键词后的提示信息，让机器人更加有个性和表情。
+- 🚀 你可以使用 koishi 的配置界面来方便地设置这些参数，也可以直接联系我来实现更多的功能。
+
+## 📦 安装
+
+```
+前往 Koishi 插件市场添加该插件即可
+```
+
+## 📝 命令
+
+```
+本插件未提供指令
+```
+
+## 🎮 使用
+
+- 🎉 启动 koishi，并享受和机器人的互动吧！
+
+## ❗️ 注意事项
+
+- ❗️ 本插件只能过滤用户输入的命令参数（args）。
+- ❗️ 本插件只能过滤文本类型的参数。
+
+## 🙏 致谢
+
+* [Koishi](https://koishi.chat/)：机器人框架
+* [melinoe](https://forum.koishi.xyz/t/topic/4578)：喵 miaO~
+
+## 📄 License
+
+MIT License © 2023
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,103 @@
 import { Context, Schema } from 'koishi'
 
 export const name = 'command-keyword-filter'
+export const usage = `## ❗️ 注意事项
 
-export interface Config {}
+- ❗️ 本插件只能过滤用户输入的命令参数（args）。
+- ❗️ 本插件只能过滤文本类型的参数。`
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  keywords: string[]; // 关键词
+  action: any; // 触发关键词后做的动作
+  timeLimit: number; // 触发时间限制（秒）
+  triggerMessage: string; // 触发关键词后的提示信息
+  bannedMessage: string; // 被屏蔽后的提示信息
+  reminderMessage: string; // 触发关键词的提示信息（仅提示不屏蔽）
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  keywords: Schema.array(String).role('table').description('过滤关键词'),
+  action: Schema.union(['仅封印无提示', '仅提示', '既封印又提示']).default('既封印又提示').description('触发关键词后做的动作'),
+  timeLimit: Schema.number().default(60).description('触发时间限制（秒）'),
+  triggerMessage: Schema.string().role('textarea', { rows: [1, 4] }).default('你一点都不可爱喵~ 从现在开始我要讨厌你一会儿啦~ 略略略~').description('触发关键词后的提示信息'),
+  bannedMessage: Schema.string().role('textarea', { rows: [1, 4] }).default('哼~ 我还在生气呢~ 叫你惹我生气！凶你喵~！《剩余时间》 秒后再来找我玩吧~').description('被屏蔽后的提示信息（文本中的《剩余时间》将会被替换成实际剩余时间的秒数）'),
+  reminderMessage: Schema.string().role('textarea', { rows: [1, 4] }).default('我警告你喵~ 别再惹我生气啦~ 否则的话，我会生气的！（拿起小拳头对你挥了挥喵~）').description('触发关键词的提示信息（仅提示不屏蔽）'),
+})
+
+// 定义一个 Map 类型的容器，用来存储 session.userId 和触发的时间
+const container = new Map<string, number>();
+
+export function apply(ctx: Context, config: Config) {
+  const {
+    keywords,
+    action,
+    timeLimit,
+    triggerMessage,
+    bannedMessage,
+    reminderMessage,
+  } = config;
+
+  ctx.command('t').action(async ({ session }) => {
+    await session.send('6');
+  });
+
+  // 监听 command/before-execute 事件
+  ctx.on('command/before-execute', async (argv) => {
+    // 调用 checkArgs 函数，判断 args 是否包含 keywords
+    const result = checkArgs(argv.args, keywords);
+    // 获取当前时间戳，单位为毫秒
+    const now = Date.now();
+
+    // 如果容器中已经有了 session.userId 的记录
+    if (container.has(argv.session.userId)) {
+      // 获取之前存储的时间戳
+      const prev = container.get(argv.session.userId);
+      // 计算时间差值，单位为秒
+      const diff = calculateTimeDifference(now, prev);
+
+      // 如果时间差值小于 time
+      if (diff < timeLimit) {
+        if (action === '仅封印无提示') {
+          return '';
+        }
+        return bannedMessage.replace('《剩余时间》', `${Math.floor(timeLimit - diff)}`);
+      }
+    }
+
+    // 如果结果为 true
+    if (result) {
+      if (action === '仅提示') {
+        return reminderMessage;
+      }
+
+      // 如果容器中没有 session.userId 的记录，或者时间差值大于等于 time
+      // 将 session.userId 和当前时间戳存入容器中
+      container.set(argv.session.userId, now);
+
+      // if (action === '仅封印无提示') {
+      //   return '';
+      // }
+
+      return triggerMessage;
+    }
+  });
+
+  // 定义一个函数，用来检查 args 是否包含 keywords
+  function checkArgs(args: string[], keywords: string[]): boolean {
+    // 使用 some 方法，检查 args 数组是否有至少一个元素满足条件
+    return args.some((arg) => {
+      // 使用 some 方法，检查 keywords 数组是否有至少一个元素包含在 arg 中
+      return keywords.some((keyword) => {
+        // 使用 includes 方法，检查 arg 是否包含 keyword
+        return arg.includes(keyword);
+      });
+    });
+  }
+
+
+
+  // 定义一个函数，用来计算时间差值（单位：秒）
+  function calculateTimeDifference(current: number, previous: number): number {
+    return Math.floor((current - previous) / 1000);
+  }
 }
